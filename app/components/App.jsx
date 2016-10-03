41b5d885f86a5b5881d56f53c36806d6bfa1eef1
Frederik Leidloff <kontakt@frederik-leidloff.de>
2016-10-03 21:25:11 +0200

    add score card

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Game from './Game';
+import ScoreCard from "./Scorecard";
 
 export default class App extends React.Component {
   render() {
     return (
       <div id="content">
+        <ScoreCard />
         <Game />
+
       </div>
     );
   }
---
+++ app/components/Scorecard.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+export default () => (
+	<div className="ScoreCard">
+		3 point
+		<div className="solution">
+			<div className="black" />
+			<div className="black" />
+			<div className="black" />
+			<div className="black" />
+		</div>
+		<div className="solution">
+			<div className="black" />
+			<div className="black" />
+			<br />
+			<div className="black" />
+			<div className="black" />
+		</div>
+		<div className="solution">
+			<div className="black" />
+			<div className="white" />
+			<div className="white" />
+			<div className="white" />
+			<br />
+			<div className="white" />
+			<div className="black" />
+			<div className="white" />
+			<div className="white" />
+			<br />
+			<div className="white" />
+			<div className="white" />
+			<div className="black" />
+			<div className="white" />
+			<br />
+			<div className="white" />
+			<div className="white" />
+			<div className="white" />
+			<div className="black" />
+		</div>
+		5 points
+		<div className="solution">
+			<div className="white" />
+			<div className="black" />
+			<div className="white" />
+			<br />
+			<div className="black" />
+			<div className="black" />
+			<div className="black" />
+			<br />
+			<div className="white" />
+			<div className="black" />
+			<div className="white" />
+		</div>
+		<div className="solution">
+			<div className="white" />
+			<div className="white" />
+			<div className="white" />
+			<div className="black" />
+			<br />
+			<div className="black" />
+			<div className="black" />
+			<div className="black" />
+			<div className="black" />
+		</div>
+	</div>
+);
